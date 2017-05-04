b7c9fef002d1d7c35721bfaedd04e7868c789fbd
CARMEN\Carmen <cirebea.carmen@gmail.com>
2017-05-04 19:21:38 +0300

    Speaker domain class

---
+++ Domain/Speaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+    public class Speaker:User
+    {
+        //base constructor
+        public Speaker() { }
+
+        //parameterized constructor
+        public Speaker(string username,string password,string firstName,string surName,string email):
+            base(username, password, firstName, surName, email)
+        { }
+        public string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
