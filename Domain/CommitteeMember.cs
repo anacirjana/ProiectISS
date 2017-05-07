6617c275cc239813e216e31d609bef51496731d8
ChisAntonia <chis.antonia2003@gmail.com>
2017-05-07 10:52:34 +0300

    CommitteeMember Domain Class

---
+++ Domain/CommitteeMember.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+    class CommitteeMember: User
+    {
+        private string affiliation;
+        private string webpage;
+        private string role;
+
+        public CommitteeMember(string uname, string pass, string fname, string lname, string eml, string afl, string web, string role):
+            base(uname,pass,fname,lname,eml)
+        {
+            this.affiliation = afl;
+            this.webpage = web;
+            this.role = role;
+        }
+
+        public string Affiliation
+        {
+            get
+            {
+                return this.affiliation;
+            }
+
+            set
+            {
+                this.affiliation = value;
+            }
+        }
+
+        public string Webpage
+        {
+            get
+            {
+                return this.webpage;
+            }
+
+            set
+            {
+                this.webpage = value;
+            }
+        }
+
+        public string Role
+        {
+            get
+            {
+                return this.role;
+            }
+
+            set
+            {
+                this.role = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + this.affiliation + this.webpage + this.role;
+        }
+    }
+}
