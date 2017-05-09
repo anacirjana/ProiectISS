a2efdf5df0e012ad5df3582eea4f19da55b4e4c6
HoteaCatalin <catalinhotea11@yahoo.com>
2017-05-09 13:00:22 +0300

    Repository source code

---
+++ Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ISS.Repository
+    {
+        public interface IRepository<ID, E>
+        {
+            void Save(E elem);
+            void Delete(ID id);
+            E GetOne(ID id);
+            IEnumerable<E> GetAll();
+            void Update(E e1, E e2);
+        }
+    }
+
+}
