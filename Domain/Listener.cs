618d7ad2466a75123687e5c73c353118bb0e30f0
Ana Cirjan <ana_cirjan@yahoo.com>
2017-05-16 20:31:40 +0300

    clasa Listener, care mosteneste User

---
+++ Domain/Listener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+        public class Listener : User
+        {
+            //base constructor
+            public Listener() { }
+
+            //parameterized constructor
+            public Listener(string username, string password, string firstName, string surName, string email) :
+                base(username, password, firstName, surName, email)
+            { }
+            public string ToString()
+            {
+                return base.ToString();
+            }
+        }
+    }
+}
