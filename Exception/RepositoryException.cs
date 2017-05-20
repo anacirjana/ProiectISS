223cc9cfd5520dcc255a9c5ff64a14247f207975
CARMEN\Carmen <cirebea.carmen@gmail.com>
2017-05-20 19:47:51 +0300

    Clase pentru Abstract Repository si Exception

---
+++ Exception/RepositoryException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ISS.Exception
+    {
+        public class RepositoryException : ApplicationException
+        {
+            public RepositoryException(string msg)
+
+            : base(msg) { }
+
+        }
+    }
+}
---
+++ Repository/ARepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace ISS.Repository
+    {
+       public abstract class ARepository<ID,E>:IRepository<ID,E>
+        {
+            IDictionary<ID, E> items;
+            public ARepository(IValidator<E> validator)
+            {
+                items = new Dictionary<ID, E>();
+            }
+            public AbsRepo()
+            {
+                items = new Dictionary<ID, T>();
+            }
+
+
+            public virtual void Delete(ID id)
+            {
+                items.Remove(id);
+
+            }
+            public virtual void update(E entityold, E entity)
+            {
+                items[entityold.Id]= entity;
+            }
+
+            public virtual IEnumerable<E> GetAll()
+            {
+                return items.Values;
+            }
+
+            public virtual E GetOne(ID id)
+            {
+
+                if (items.ContainsKey(id))
+                    return items[id];
+                else
+                    return default(E);
+
+            }
+
+            public virtual void Save(E entity)
+            {
+                if (!items.ContainsKey(entity.Id))
+                    items.Add(entity.Id, entity);
+                else throw new RepositoryException("Duplicate entity " + entity);
+            }
+        }
+}
