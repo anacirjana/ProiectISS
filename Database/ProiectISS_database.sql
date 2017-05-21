d779596c9241183a19c397d4de2610ba60127a78
HoteaCatalin <catalinhotea11@yahoo.com>
2017-05-21 14:45:53 +0300

    Database for project

---
+++ Database/ProiectISS_database.sql
@@ -0,0 +1,60 @@
+use master
+if exists( select * from sys.databases where name='ProiectISS' )
+	drop database ProiectISS
+create database ProiectISS
+
+go
+use ProiectISS
+
+create table CommitteeMembers(
+	username nvarchar(10) primary key,
+	pass nvarchar(10),
+	firstName nvarchar(10),
+	sureName nvarchar(10),
+	email nvarchar(30),
+	webPage nvarchar(20),
+	affiliation nvarchar(15),
+	rang nvarchar(10)
+);
+
+create table Listeners(
+	username nvarchar(10) primary key,
+	pass nvarchar(10),
+	firstName nvarchar(10),
+	sureName nvarchar(10)
+);
+
+create table Sections(
+	idS int primary key,
+	numeS nvarchar(12)
+);
+
+create table Papers(
+	idP int primary key,
+	titlu nvarchar(20),
+	keywords nvarchar(20),
+	topics nvarchar(20),
+	authors nvarchar(50),
+	publisher nvarchar(20),
+	datePaper date,
+	pathPaper nvarchar(30),
+	idSection int references Sections(idS)
+
+);
+
+
+create table Speakers(
+	username nvarchar(10) primary key,
+	pass nvarchar(10),
+	firstName nvarchar(10),
+	sureName nvarchar(10),
+	idLucrare int foreign key references Papers(idP)
+);
+
+create table Reviews(
+	idR int primary key,
+	username nvarchar(10) references CommitteeMembers(username),
+	idP int references Papers(idP),
+	qualifier nvarchar(20) ,
+	comment nvarchar(100)
+);
