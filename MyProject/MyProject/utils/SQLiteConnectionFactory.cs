2c9178c1c4bffbd20b7eabb000e5b7760feef86a
Ana Cirjan <ana_cirjan@yahoo.com>
2017-05-26 14:51:26 +0300

    Utile pt baza de date, sa facem o singura conexiune

---
+++ MyProject/MyProject/utils/SQLiteConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+namespace Lab2Agentii.utils
+{
+    class SQLiteConnectionFactory : ConnectionFactory
+    {
+        public override IDbConnection createConnection()
+        {
+            String connectionString = "Data Source=BazaDate.db;Version=3;";
+            return new SQLiteConnection(connectionString);
+        }
+    }
+}
