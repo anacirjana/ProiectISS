0687f946349e6045a334f471b3ef214f36a8727d
CARMEN\Carmen <cirebea.carmen@gmail.com>
2017-05-26 15:28:20 +0300

    no message

---
+++ MyProject/MyProject/Utils/DBUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.utils
+{
+        public static class DBUtils
+        {
+            private static IDbConnection instance = null;
+
+            public static IDbConnection getConnection()
+            {
+                if (instance == null || instance.State == System.Data.ConnectionState.Closed)
+                {
+                    instance = getNewConnection();
+                    instance.Open();
+                }
+                return instance;
+            }
+
+            private static IDbConnection getNewConnection()
+            {
+                return ConnectionFactory.getInstance().createConnection();
+            }
+        }
+    
+}
