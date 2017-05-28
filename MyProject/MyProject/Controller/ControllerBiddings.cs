72c0cff1a918fd4b6da4377422f92a2875a0d082
ChisAntonia <chis.antonia2003@gmail.com>
2017-05-28 14:31:20 +0300

    Controller Biddings

---
+++ MyProject/MyProject/Controller/ControllerBiddings.cs
@@ -0,0 +1,47 @@
+﻿using MyProject.Domain;
+using MyProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Controller
+{
+    class ControllerBiddings
+    {
+        private RepositoryBiddings _repositoryBidding;
+
+        public ControllerBiddings()
+        {
+            _repositoryBidding = new RepositoryBiddings();
+        }
+
+        public void addBidding(Bidding bidding)
+        {
+            _repositoryBidding.Save(bidding);
+        }
+
+        public void deleteBidding(string userCM, int idPaper)
+        {
+            string idBidding = userCM + " " + idPaper.ToString();
+            _repositoryBidding.Delete(idBidding);
+        }
+
+        public void updateBidding(Bidding oldBidding, Bidding newBidding)
+        {
+            _repositoryBidding.Update(oldBidding, newBidding);
+        }
+
+        public Bidding getOneBidding(string userCM, int idPaper)
+        {
+            string idBidding = userCM + " " + idPaper.ToString();
+            return _repositoryBidding.GetOne(idBidding);
+        }
+
+        public IEnumerable<Bidding> getAllBiddings()
+        {
+            return _repositoryBidding.GetAll();
+        }
+    }
+}
