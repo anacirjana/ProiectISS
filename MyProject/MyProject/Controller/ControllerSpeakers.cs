e8b7b5ebe1c80f97aea4fa9f647359fdf7eb25fa
DESKTOP-NVKMTUP\Cata <catalinhotea11@yahoo.com>
2017-05-28 20:39:49 +0300

    Controller speakeeer

---
+++ MyProject/MyProject/Controller/ControllerSpeakers.cs
@@ -0,0 +1,33 @@
+﻿using MyProject.Domain;
+using MyProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Controller
+{
+    class ControllerSpeakers
+    {
+        RepositorySpeakers repo;
+
+        public ControllerSpeakers(RepositorySpeakers r)
+        {
+            repo = r;
+        }
+
+        public Speaker Login(string username, string password)
+        {
+            Speaker s = repo.GetOne(username);
+            if (s.Password == password)
+                return s;
+            else
+                return new Speaker("", "", "", "", "");
+
+            Speaker sS=new Speaker();
+            return sS;
+
+        }
+    }
+}
