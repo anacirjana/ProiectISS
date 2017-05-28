be78b14e66cd2c6836319ce9747717fc3302a58e
MihaiCampeanu <mihaicampeanu2796@gmail.com>
2017-05-28 19:44:01 +0300

    Add files via upload

---
+++ MyProject/MyProject/Domain/Abstract.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Domain
+{
+     class Abstract
+    {
+        private Int32 idP;
+        private String pathAbstract;
+        
+        public Abstract
+        {
+            idP = 0;
+            pathAbstract="";   
+        }
+
+       public  Int32 Id
+        {
+            get
+            {
+                return this.idP;
+            }
+                
+                
+            set
+            {
+                idP = value;
+            }
+
+        }
+
+        public  String Path
+        {
+            get
+            {
+                return pathAbstract;
+            }
+
+            set
+            {
+                pathAbstract = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.idP + " " + this.pathAbstract;
+        }
+    }
+}
