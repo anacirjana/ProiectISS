8fa5f2927cb71617adbcd3d4116198d6256b1756
MihaiCampeanu <mihaicampeanu2796@gmail.com>
2017-05-28 19:23:48 +0300

    Add files via upload

---
+++ Domain/Abstract.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+     class Abstract
+    {
+        private Int32 idP;
+        private String pathAbstract;
+        
+       public  Int32 Id
+        {
+            get
+            {
+                return this.idP;
+            }
+                
+                
+            set
+            {
+                idP = value;
+            }
+
+        }
+
+        public  String Path
+        {
+            get
+            {
+                return pathAbstract;
+            }
+
+            set
+            {
+                pathAbstract = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.idP + " " + this.pathAbstract;
+        }
+    }
+}
