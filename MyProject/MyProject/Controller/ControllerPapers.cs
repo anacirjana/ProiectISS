fab9233348d5c12d65d31e8d0b076f2d9fdf1798
CARMEN\Carmen <cirebea.carmen@gmail.com>
2017-05-28 22:35:16 +0300

    ControllerPapers done

--- MyProject/MyProject/Controller/ControllerPapers.cs
+++ MyProject/MyProject/Controller/ControllerPapers.cs
@@ -15,9 +15,67 @@ namespace MyProject.Controller
         {
             repop = new RepositoryPapers();
         }
-        public IEnumerable<Paper> getAllPapers()
+        public ControllerPapers(RepositoryPapers repop)
+        {
+            this.repop = repop;
+        }
+        public void Update(Paper paperOld, Paper paperNew)
+        {
+            repop.Update(paperOld, paperNew);
+        }
+        public void Delete(int id)
+        {
+            repop.Delete(id);
+        }
+
+        public Paper GetPaper(int id)
+        {
+            return repop.GetOne(id);
+        }
+
+        public IEnumerable<Paper> GetAllPapers()
         {
             return repop.GetAll();
+        }
+
+        public string GetTitle(int id)
+        {
+            return repop.GetTitle(id);
+        }
+
+        public string GetKeywords(int id)
+        {
+            return repop.GetKeywords(id);
+        }
+
+        public string GetTopic(int id)
+        {
+            return repop.GetTopic(id);
+        }
+
+        public string GetAuthors(int id)
+        {
+            return repop.GetAuthors(id);
+        }
+
+        public string GetPublisher(int id)
+        {
+            return repop.GetPublisher(id);
+        }
+
+        public int GetDate(int id)
+        {
+            return repop.GetDate(id);
+        }
+
+        public string GetPathPaper(int id)
+        {
+            return repop.GetPathPaper(id);
+        }
+
+        public int GetidLucrare(int id)
+        {
+            return repop.GetidLucrare(id);
         }
 
     }
