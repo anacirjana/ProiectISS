ae2c31e41622f09c0eec08ff425302713fe84d68
MihaiCampeanu <mihaicampeanu2796@gmail.com>
2017-05-28 21:21:39 +0300

    Add files via upload

---
+++ MyProject/MyProject/Controller/ControllerListeners.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ControllerListeners
+    {
+        private RepositoryListeners repo;
+
+        public ControllerListeners(RepositoryListeners r)
+        {
+            repo = new RepositorySpeakers();
+        }
+
+        public Listener Login(string username, string password)
+        {
+            Listener s = repo.GetOne(username);
+            if (s.Password == password)
+                return s;
+            else
+                return new Listener("", "", "", "", "");
+
+        }
+
+        public void Register(Listener s)
+        {
+            repo.Save(s);
+        }
+
+        public void deleteListener(string username)
+        {
+
+            repo.Delete(username);
+        }
+
+        public void updateListener(Listener oldListener, Listener newListener)
+        {
+            repo.Update(oldListener, newListener);
+        }
+
+        public Listener getOneListener(string username)
+        {
+
+            return repo.GetOne(username);
+        }
+
+        public IEnumerable<Listener> getAllListener()
+        {
+            return repo.GetAll();
+        }
+
+
+    }
+}
