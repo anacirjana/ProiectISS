546d427f20360f9a691f7b666bfe5b0229c7b16c
Ana Cirjan <ana_cirjan@yahoo.com>
2017-06-01 17:55:37 +0300

    made form speaker

---
+++ MyProject/MyProject/FormSpeaker.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MyProject
+{
+    partial class FormSpeaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.LabelUsername = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(23, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(430, 63);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Welcome to CMS,";
+            // 
+            // LabelUsername
+            // 
+            this.LabelUsername.AutoSize = true;
+            this.LabelUsername.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelUsername.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.LabelUsername.Location = new System.Drawing.Point(474, 22);
+            this.LabelUsername.Name = "LabelUsername";
+            this.LabelUsername.Size = new System.Drawing.Size(258, 63);
+            this.LabelUsername.TabIndex = 3;
+            this.LabelUsername.Text = "username";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 15F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(34, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(483, 47);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Upload Paper from my Computer";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(543, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 22);
+            this.textBox1.TabIndex = 22;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 15F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(34, 236);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(483, 47);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Upload Abstract from my Computer";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(543, 252);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(337, 22);
+            this.textBox2.TabIndex = 24;
+            // 
+            // FormSpeaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(932, 503);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LabelUsername);
+            this.Controls.Add(this.label2);
+            this.Name = "FormSpeaker";
+            this.Text = "FormSpeakers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LabelUsername;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ MyProject/MyProject/FormSpeaker.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using MyProject.Domain;
+namespace MyProject
+{
+    public partial class FormSpeaker : Form
+    {
+        Speaker loggedUser;
+        public FormSpeaker(Speaker s)
+        {
+            loggedUser = s;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op1 = new OpenFileDialog();
+
+            op1.Multiselect = true;
+
+            op1.ShowDialog();
+
+            op1.Filter = "allfiles|*.doc*";
+
+            textBox1.Text = op1.FileName;
+
+            int count = 0;
+
+            string[] FName;
+
+            foreach (string s in op1.FileNames)
+
+            {
+
+                FName = s.Split('\\');
+                string ExecutableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                object path = Path.Combine(ExecutableLocation, "PaperContents/");
+                File.Copy(s, path + FName[FName.Length - 1]);
+
+                count++;
+
+            }
+
+            MessageBox.Show(Convert.ToString(count) + "Uploaded with success!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op1 = new OpenFileDialog();
+
+            op1.Multiselect = true;
+
+            op1.ShowDialog();
+
+            op1.Filter = "allfiles|*.docx";
+
+            textBox1.Text = op1.FileName;
+
+            int count = 0;
+
+            string[] FName;
+
+            foreach (string s in op1.FileNames)
+
+            {
+
+                FName = s.Split('\\');
+                string ExecutableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                object path = Path.Combine(ExecutableLocation, "PaperContents/");
+                File.Copy(s, path + FName[FName.Length - 1]);
+
+                count++;
+
+            }
+        }
+    }
+}
