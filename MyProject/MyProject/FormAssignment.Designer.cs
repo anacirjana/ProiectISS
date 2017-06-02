1a71ebff2ba19e759c597713f3c0e08afb8acaf2
DESKTOP-NVKMTUP\Cata <catalinhotea11@yahoo.com>
2017-06-02 18:27:39 +0300

    Form Assignment doneeeeee

---
+++ MyProject/MyProject/FormAssignment.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MyProject
+{
+    partial class FormAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(31, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Assignments";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(122, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Papers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(508, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Committee Members";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(42, 138);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(250, 260);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(477, 138);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(261, 259);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 15F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(316, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 47);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Assign";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FromAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 461);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FromAssignment";
+            this.Text = "FromAssignment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ MyProject/MyProject/FormAssignment.cs
@@ -0,0 +1,86 @@
+﻿using MyProject.Controller;
+using MyProject.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProject
+{
+    public partial class FormAssignment : Form
+    {
+        ControllerPapers ctrp;
+        ControllerBiddings ctrb;
+        ControllerReviews ctrr;
+        
+      
+
+        public FormAssignment()
+        {
+            InitializeComponent();
+            ctrp = new ControllerPapers();
+            List<Paper> paperList = (List<Paper>)ctrp.GetAllPapers();
+            foreach (Paper p in paperList)
+            {
+                listBox1.Items.Add(p.IdP.ToString() + '.' + p.Title);
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+            ctrb = new ControllerBiddings();
+            List<Bidding> biddingList = (List<Bidding>)ctrb.getAllBiddings();
+            string t = (string)(listBox1.SelectedItem);
+            string[] res = t.Split('.');
+            int id;
+            int.TryParse(res[0], out id);
+            foreach(Bidding b in biddingList)
+            {
+                if (b.IdPaper == id && b.Accepted == true)
+                    checkedListBox1.Items.Add(b.UsernameCommitteeMember);
+
+
+            }
+            
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        //assign
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ctrr = new ControllerReviews();
+            foreach (object itemChecked in checkedListBox1.CheckedItems)
+            {
+                string username = itemChecked.ToString();
+              
+                string t = (string)(listBox1.SelectedItem);
+                string[] res = t.Split('.');
+                int id;
+                int.TryParse(res[0], out id);
+             
+                ctrr.AddReview(new Review(username, id, "", ""));
+                
+                MessageBox.Show("Assignment made to "+ username);
+            }
+
+            
+
+            //clear checked boxes
+            foreach (int i in checkedListBox1.CheckedIndices)
+            {
+                checkedListBox1.SetItemCheckState(i, CheckState.Unchecked);
+            }
+        }
+    }
+}
