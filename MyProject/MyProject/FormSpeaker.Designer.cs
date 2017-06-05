920643b3169b764e9b927984713b92e9e5592e0e
Ana Cirjan <ana_cirjan@yahoo.com>
2017-06-05 21:30:23 +0300

    form speaker done

--- MyProject/MyProject/FormSpeaker.Designer.cs
+++ MyProject/MyProject/FormSpeaker.Designer.cs
@@ -35,28 +35,42 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.ComboBoxParticipants = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 27F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
             this.label2.Location = new System.Drawing.Point(23, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(430, 63);
+            this.label2.Size = new System.Drawing.Size(389, 57);
             this.label2.TabIndex = 2;
             this.label2.Text = "Welcome to CMS,";
             // 
             // LabelUsername
             // 
             this.LabelUsername.AutoSize = true;
-            this.LabelUsername.Font = new System.Drawing.Font("Trebuchet MS", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LabelUsername.Font = new System.Drawing.Font("Trebuchet MS", 27F, System.Drawing.FontStyle.Bold);
             this.LabelUsername.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.LabelUsername.Location = new System.Drawing.Point(474, 22);
+            this.LabelUsername.Location = new System.Drawing.Point(434, 22);
             this.LabelUsername.Name = "LabelUsername";
-            this.LabelUsername.Size = new System.Drawing.Size(258, 63);
+            this.LabelUsername.Size = new System.Drawing.Size(232, 57);
             this.LabelUsername.TabIndex = 3;
             this.LabelUsername.Text = "username";
             // 
@@ -65,18 +79,18 @@
             this.button1.BackColor = System.Drawing.Color.DarkSlateGray;
             this.button1.Font = new System.Drawing.Font("Trebuchet MS", 15F);
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button1.Location = new System.Drawing.Point(34, 126);
+            this.button1.Location = new System.Drawing.Point(579, 171);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(483, 47);
+            this.button1.Size = new System.Drawing.Size(252, 47);
             this.button1.TabIndex = 21;
-            this.button1.Text = "Upload Paper from my Computer";
+            this.button1.Text = "(Re)Upload Paper";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(543, 142);
+            this.textBox1.Location = new System.Drawing.Point(543, 116);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(337, 22);
             this.textBox1.TabIndex = 22;
@@ -86,18 +100,18 @@
             this.button2.BackColor = System.Drawing.Color.DarkSlateGray;
             this.button2.Font = new System.Drawing.Font("Trebuchet MS", 15F);
             this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.button2.Location = new System.Drawing.Point(34, 236);
+            this.button2.Location = new System.Drawing.Point(579, 322);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(483, 47);
+            this.button2.Size = new System.Drawing.Size(252, 47);
             this.button2.TabIndex = 23;
-            this.button2.Text = "Upload Abstract from my Computer";
+            this.button2.Text = "(Re)Upload Abstract";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(543, 252);
+            this.textBox2.Location = new System.Drawing.Point(543, 278);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(337, 22);
             this.textBox2.TabIndex = 24;
@@ -107,12 +121,161 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = global::MyProject.Properties.Resources.inapoi;
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(21, 435);
+            this.pictureBox1.Location = new System.Drawing.Point(1, 499);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(61, 56);
             this.pictureBox1.TabIndex = 25;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(222, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(213, 22);
+            this.textBox3.TabIndex = 26;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.MintCream;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label5.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label5.Location = new System.Drawing.Point(76, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 32);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Title:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.MintCream;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(17, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 32);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "Keywords:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.MintCream;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label3.Location = new System.Drawing.Point(59, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 32);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Topics:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(220, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(213, 22);
+            this.textBox4.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(218, 234);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(213, 22);
+            this.textBox5.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MintCream;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Location = new System.Drawing.Point(43, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 32);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Authors:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(218, 295);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(213, 22);
+            this.textBox6.TabIndex = 33;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.MintCream;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label6.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label6.Location = new System.Drawing.Point(27, 350);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 32);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Publisher:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(216, 354);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(213, 22);
+            this.textBox7.TabIndex = 35;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(216, 411);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(213, 22);
+            this.textBox8.TabIndex = 36;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.MintCream;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label7.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label7.Location = new System.Drawing.Point(81, 404);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 32);
+            this.label7.TabIndex = 37;
+            this.label7.Text = "Date:";
+            // 
+            // ComboBoxParticipants
+            // 
+            this.ComboBoxParticipants.BackColor = System.Drawing.Color.AliceBlue;
+            this.ComboBoxParticipants.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ComboBoxParticipants.DropDownHeight = 130;
+            this.ComboBoxParticipants.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxParticipants.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ComboBoxParticipants.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold);
+            this.ComboBoxParticipants.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.ComboBoxParticipants.FormattingEnabled = true;
+            this.ComboBoxParticipants.IntegralHeight = false;
+            this.ComboBoxParticipants.Items.AddRange(new object[] {
+            "Medicine",
+            "Math&CS",
+            "Environment"});
+            this.ComboBoxParticipants.Location = new System.Drawing.Point(218, 457);
+            this.ComboBoxParticipants.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ComboBoxParticipants.Name = "ComboBoxParticipants";
+            this.ComboBoxParticipants.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ComboBoxParticipants.Size = new System.Drawing.Size(212, 31);
+            this.ComboBoxParticipants.TabIndex = 38;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.MintCream;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label8.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label8.Location = new System.Drawing.Point(-5, 456);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(198, 32);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "Choose section:";
             // 
             // FormSpeaker
             // 
@@ -120,7 +283,21 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MintCream;
             this.BackgroundImage = global::MyProject.Properties.Resources.cm;
-            this.ClientSize = new System.Drawing.Size(932, 503);
+            this.ClientSize = new System.Drawing.Size(961, 556);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.ComboBoxParticipants);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button2);
@@ -147,5 +324,19 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox ComboBoxParticipants;
+        private System.Windows.Forms.Label label8;
     }
 }
